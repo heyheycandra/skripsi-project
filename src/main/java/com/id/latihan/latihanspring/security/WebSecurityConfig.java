b8ccbf6e8aae7eafc909ddc87069fe53537821e5
Odi Saputra <Saputraodi393@gmail.com>
2022-03-29 10:32:22 +0700

    comitt wecSecurity

--- src/main/java/com/id/latihan/latihanspring/security/WebSecurityConfig.java
+++ src/main/java/com/id/latihan/latihanspring/security/WebSecurityConfig.java
@@ -66,8 +66,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
 				.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-//				.antMatchers("/api/auth/**").permitAll()
-//			.antMatchers("/api/test/**").permitAll()
+				.antMatchers("/api/auth/**").permitAll()
+			.antMatchers("/api/test/**").permitAll()
 			.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
