2b2c7396e0983d8746f0a39f98f38dabba551912
Odi Saputra <Saputraodi393@gmail.com>
2022-04-05 10:19:06 +0700

    cors

--- src/main/java/com/id/latihan/latihanspring/LatihanSpringApplication.java
+++ src/main/java/com/id/latihan/latihanspring/LatihanSpringApplication.java
@@ -14,5 +14,15 @@ public class LatihanSpringApplication {
         SpringApplication.run(LatihanSpringApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("*");
+            }
+
+        };
+    }
 
 }
